50317b053e43d618628eff3c46115be554da2735
ArlandoIfrene <arlandoifrene@gmail.com>
2024-03-08 21:22:52 -0300

    Here we go!

--- front/scripts/signIn.js
+++ front/scripts/signIn.js
@@ -14,12 +14,12 @@ const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     // Check if the user provided an email
     if (isValidEmail(email)) {
       // Display an alert with a message
-      alert('Thank you for signing in. You will receive news via email at ' + email);
+      alert('Thank you for signing up. \nYou will receive news via email at ' + email);
       emailRepository.push(email);
 
     } else {
       // Display an alert if the user canceled or did not provide an email
-      alert('Incorrec email address format! ');
+      alert('Incorrect email address format! ');
     }
 
     console.log(emailRepository);
